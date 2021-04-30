ca730c141e39f8768812007b40e24fc14f72da4a
Hui-Sin <20018354@myrp.edu.sg>
2021-04-30 10:03:07 +0800

    Added Comments to MainActivity

---
+++ app/src/main/java/sg/edu/rp/c346/id20018354/myfirstgit/MainActivity.java
@@ -0,0 +1,16 @@
+package sg.edu.rp.c346.id20018354.myfirstgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    //This is a new line
+    }
+}
